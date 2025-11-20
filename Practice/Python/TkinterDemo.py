c1d964729728092ca94dbe275fcb2d9556a2d6d2
Lodhiakash12 <akashlodhi7836@gmail.com>
2025-11-20 07:06:39 -0800

    Module 1

--- Practice/Python/TkinterDemo.py
+++ Practice/Python/TkinterDemo.py
@@ -28,6 +28,10 @@ def insert_data():
         conn.close()
 
 def search_data():
+    e_fname.delete(0,'end')
+    e_lname.delete(0,'end')
+    e_email.delete(0,'end')
+    e_mobile.delete(0,'end')
     if e_id.get()=="":
         msg.showinfo("Search Status","ID Required")
     else:
@@ -37,9 +41,57 @@ def search_data():
         query="select * from student where id=%s"
         args=(e_id.get(),)
         cursor.execute(query,args)
-        conn.commit()
-         
+        row=cursor.fetchall()
+        print(row)
+        if row:
+            e_fname.insert(0,row[0][1])
+            e_lname.insert(0,row[0][2])
+            e_email.insert(0,row[0][3])
+            e_mobile.insert(0,row[0][4])
+        else:
+            e_fname.delete(0,'end')
+            e_lname.delete(0,'end')
+            e_email.delete(0,'end')
+            e_mobile.delete(0,'end')
+            msg.showinfo("Search Status","Id Not Found")
         conn.close()
+
+def update_data():
+    if e_fname.get()=="" or e_lname.get()=="" or e_email.get()=="" or e_mobile.get()=="":
+        msg.showinfo("Update Status","All Field Required")
+    else:
+        conn=create_conn()
+        cursor=conn.cursor()
+        query="update student set fname=%s,lname=%s,email=%s,mobile=%s"
+        args=(e_fname.get(),e_lname.get(),e_email.get(),e_mobile.get())
+        cursor.execute(query,args)
+        conn.commit()
+        conn.close()
+        e_fname.delete(0,'end')
+        e_lname.delete(0,'end')
+        e_email.delete(0,'end')
+        e_mobile.delete(0,'end')
+        msg.showinfo("Update Status","Successfully Updated")
+
+def delete_data():
+    if e_id.get()=="":
+        msg.showinfo("Delete Status","Id Field Required")
+    else:
+        conn=create_conn()
+        cursor=conn.cursor()
+        query="delete from student where id=%s"
+        args=(e_id.get(),)
+        cursor.execute(query,args)
+        conn.commit()
+        conn.close()
+        e_fname.delete(0,'end')
+        e_lname.delete(0,'end')
+        e_email.delete(0,'end')
+        e_mobile.delete(0,'end')
+        msg.showinfo("Delete Status","Data Deleted Successfully")
+        
+        
+    
         
     
 
@@ -87,8 +139,8 @@ insert.place(x=20,y=300)
 search=Button(root,text="SEARCH",bg="Black",fg="White",font=("Times New Roman",10),command=search_data)
 search.place(x=80,y=300)
 
-update=Button(root,text="UPDATE",bg="Black",fg="White",font=("Times New Roman",10))
+update=Button(root,text="UPDATE",bg="Black",fg="White",font=("Times New Roman",10),command=update_data)
 update.place(x=150,y=300)
 
-delete=Button(root,text="DELETE",bg="Black",fg="White",font=("Times New Roman",10))
+delete=Button(root,text="DELETE",bg="Black",fg="White",font=("Times New Roman",10),command=delete_data)
 delete.place(x=220,y=300)
